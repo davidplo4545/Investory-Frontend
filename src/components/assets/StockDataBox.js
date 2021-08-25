06efb1cb80551957d74880261318d7009f112cd5
david plotkin <davidplo4545@gmail.com>
2021-08-25 23:45:04 +0300

    Added more asset data fields received from the server

---
+++ src/components/assets/StockDataBox.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import { Grid, makeStyles, Box, Typography, useTheme, Link} from '@material-ui/core'
+const useStyles = makeStyles((theme) =>{
+    return{
+        gridItem:{
+            // border: `1px solid ${theme.palette.text.secondary}`,
+            padding: theme.spacing(2),
+            borderRadius: '15px',
+            [theme.breakpoints.down('md')]: {
+                marginTop:theme.spacing(1),
+                margin:'1rem',
+              },
+        },
+    }
+})
+
+const renderText = (header, value, symbol="") =>{
+    let result
+    if (value)
+    {
+        if(isNaN(value))
+            result = value
+        else
+        {
+            if(symbol === "")
+            {
+                // format big numbers 100,000,000 => 100M
+                if(value >= 10 ** 6 && value < 10 ** 9)
+                    result = `${(value/10 ** 6).toFixed(2)}M`
+                else if(value >= 10 ** 9 && value < 10 ** 12)
+                    result = `${(value/10 ** 9).toFixed(2)}B`
+                else if(value >= 10 ** 12)
+                    result = `${(value/10 ** 12).toFixed(2)}T`
+                else
+                    result = `${(value).toFixed(2)}`
+            }
+            else
+                result = `${value.toFixed(2)}${symbol}`
+        }
+            
+        console.log(header, value, result)
+        return(
+            <Typography variant="body2" color="textSecondary" gutterBottom>
+                {header}: <b>{result ? result : ""}</b>
+            </Typography>
+        )
+    }
+    return <Typography variant="body2" color="textSecondary" gutterBottom/>
+}
+
+const StockDataBox = ({asset}) =>{
+    const classes = useStyles()
+    const theme = useTheme()
+    return(
+        <Grid container item justifyContent="space-between"
+            alignItems={'stretch'}>
+            <Grid item xs={12} md={12} lg={3} className={classes.gridItem}>
+                <Box>
+                    <Box>
+                    <Typography variant="h6" color="textPrimary" style={{borderBottom: `1px solid ${theme.palette.text.primary}`}} gutterBottom>General:</Typography>
+                        {renderText("Type", asset.type)}
+                        {renderText("Location", asset.location)}
+                        {renderText("Sector", asset.sector)}
+                        {renderText("Industry", asset.industry)}
+                        {renderText("Website", asset.website_url)}
+                        {renderText("Full Time Employees", asset.fulltime_employees)}
+                    </Box>
+                    <Box>
+                        <Typography variant="h6" color="textPrimary" style={{borderBottom: `1px solid ${theme.palette.text.primary}`}} gutterBottom>Valuation:</Typography>
+                        {renderText("Market Cap", asset.market_cap)}
+                        {renderText("52-Week High", asset.one_year_high)}
+                        {renderText("52-Week Low", asset.one_year_low)}
+                        {renderText("Trailing P/S", asset.ps_ratio)}
+                        {renderText("Revenue Growth",asset.revenue_growth, "%")}
+                        {renderText("Trailing P/E", asset.trailing_pe)}
+                        {renderText("Forward P/E", asset.forward_pe)}
+                        {renderText("PEG Ratio", asset.peg_ratio)}
+                        {renderText("Book Value", asset.book_value)}
+                        {renderText("Price to Book", asset.price_to_book)}
+                        {renderText("Current Ratio", asset.current_ratio)}
+                        {renderText("EV", asset.enterprise_value)}
+                    </Box>
+
+                </Box>
+            </Grid>
+            <Grid item xs={12} md={12} lg={6} className={classes.gridItem}>
+                <Box>
+                    <Typography variant="h6" color="textPrimary" style={{borderBottom: `1px solid ${theme.palette.text.primary}`}} gutterBottom>Description:</Typography>
+                    <Typography variant="body2" color="textSecondary" gutterBottom>{asset.business_summary}</Typography>
+                    
+                </Box>
+            </Grid>
+            <Grid item xs={12} md={12} lg={3} className={classes.gridItem}>
+                <Box>
+                <Typography variant="h6" color="textPrimary" style={{borderBottom: `1px solid ${theme.palette.text.primary}`}} gutterBottom>Returns:</Typography>
+                {renderText("3 Month", asset.three_month_return, "%")}
+                {renderText("6 Month", asset.six_month_return, "%")}
+                {renderText("YTD", asset.ytd_return, "%")}
+                {renderText("1 Year", asset.one_year_return, "%")}
+                {renderText("3 Year", asset.three_year_return, "%")}
+                <Typography variant="h6" color="textPrimary" style={{borderBottom: `1px solid ${theme.palette.text.primary}`}} gutterBottom>Analysis:</Typography>
+                {renderText("Analysts Number", asset.num_of_analysts)}
+                {renderText("Mean Analyst Price", asset.mean_analyst_price, '$')}
+            </Box>
+            </Grid>
+    </Grid>
+    )
+}
+
+export default StockDataBox
--- src/pages/AssetPage.js
+++ src/pages/AssetPage.js
@@ -3,22 +3,10 @@ import {UserContext} from '../context/UserContext'
 import AssetAreaChart from '../components/charts/AssetAreaChart'
 import axios from 'axios'
 import RightSidebarData from '../components/assets/RightSidebarData'
-import {Box, Grid, makeStyles, Typography, useTheme} from '@material-ui/core'
+import { Grid, Typography, useTheme} from '@material-ui/core'
 import { formatNumber } from '../components/base/helpers'
+import StockDataBox from '../components/assets/StockDataBox'
 
-const useStyles = makeStyles((theme) =>{
-    return{
-        gridItem:{
-            border: `1px solid ${theme.palette.text.secondary}`,
-            padding: theme.spacing(2),
-            borderRadius: '15px',
-            [theme.breakpoints.down('md')]: {
-                marginTop:theme.spacing(1),
-                margin:'1rem',
-              },
-        },
-    }
-})
 
 const AssetPage = ({match}) =>{
     const user = useContext(UserContext)
@@ -43,8 +31,6 @@ const AssetPage = ({match}) =>{
         getAsset()
     },[assetId, user.token]);
 
-    const classes = useStyles()
-    const theme = useTheme()
     return(
         
         <React.Fragment>
@@ -52,7 +38,14 @@ const AssetPage = ({match}) =>{
             <Grid container alignItems="stretch" justifyContent="center" spacing={3} style={{marginTop:'0.5rem'}}>
                 <Grid container item lg={8} md={8} sm={12} direction="column" spacing={3}>
                     <Grid item style={{marginLeft:'1rem'}}>
-                            <Typography gutterBottom style={{fontWeight:'bold'}} variant="h4">{asset.symbol} - {asset.name}</Typography>
+                            <Typography gutterBottom style={{fontWeight:'bold', display:'flex', alignItems:'center'}} 
+                            variant="h4">
+                                {asset.logo_url &&
+                                    <img src={asset.logo_url} alt="company-logo"
+                                    style={{maxHeight:'60px', minWidth:'50px', marginRight:'1rem'}}/>
+                                }
+                                {asset.symbol} - {asset.name}
+                            </Typography>
                             <Typography color="textSecondary" variant="h5" style={{fontWeight:'bold'}}>
                                 {formatNumber(asset.last_price, asset.currency)}
                             </Typography>
@@ -60,35 +53,7 @@ const AssetPage = ({match}) =>{
                     <Grid item>
                         <AssetAreaChart asset={asset} records={asset.records}/>
                     </Grid>
-                    <Grid container item justifyContent="space-between"
-                         alignItems={'stretch'}>
-                        <Grid item xs={12} md={12} lg={4} className={classes.gridItem}>
-                            <Box>
-                                <Typography variant="h6" color="textPrimary" style={{borderBottom: `1px solid ${theme.palette.text.primary}`}} gutterBottom>Info:</Typography>
-                                <Typography variant="body2" color="textSecondary" gutterBottom>Type: {asset.type}</Typography>
-                                <Typography variant="body2" color="textSecondary" gutterBottom>Location: {asset.type}</Typography>
-                                <Typography variant="body2" color="textSecondary" gutterBottom>Sector: {asset.sector}</Typography>
-                                <Typography variant="body2" color="textSecondary" gutterBottom>Industry: {asset.industry}</Typography>
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12} md={12} lg={4} className={classes.gridItem}>
-                            <Box>
-                                <Typography variant="h6" color="textPrimary" style={{borderBottom: `1px solid ${theme.palette.text.primary}`}} gutterBottom>Description:</Typography>
-                                <Typography variant="body2" color="textSecondary" gutterBottom>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus tellus ut neque convallis pretium. Pellentesque tincidunt tellus diam, lobortis condimentum magna placerat vitae. Fusce sagittis lacus quis lorem dapibus, a eleifend neque feugiat. Vivamus eget mi dui. Aenean dolor nulla, congue sit amet pellentesque a, elementum pretium massa. Suspendisse ut ipsum sed velit blandit pretium quis id ipsum. Etiam fermentum ligula eu nunc condimentum feugiat. In non ligula eleifend, ulhiCras lacus magna, egestas id nisl luctus, facilisis consectetur erat. Aenean a elit bibendum, posuere ipsum eu, consectetur nisl.</Typography>
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12} md={12} lg={3} className={classes.gridItem}>
-                            <Box>
-                            <Typography variant="h6" color="textPrimary" style={{borderBottom: `1px solid ${theme.palette.text.primary}`}} gutterBottom>Returns:</Typography>
-                            <Typography variant="body2" color="textSecondary" gutterBottom>YTD: 5%</Typography>
-                            <Typography variant="body2" color="textSecondary" gutterBottom>1 Month: 9%</Typography>
-                            <Typography variant="body2" color="textSecondary" gutterBottom>6 Month: 9%</Typography>
-                            <Typography variant="body2" color="textSecondary" gutterBottom>1 Year: 9%</Typography>
-                            <Typography variant="body2" color="textSecondary" gutterBottom>3 Year: -5%</Typography>
-                            <Typography variant="body2" color="textSecondary" gutterBottom>5 Year: 50%</Typography>
-                            </Box>
-                        </Grid>
-                    </Grid>
+                    <StockDataBox asset={asset}/>
                 </Grid>
                 <Grid item lg={3} md={3} sm={12}>
                     {asset &&
